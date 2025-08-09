80244b4838a8f301765270c2bf5be91d922c27df
Samuel Maia <samuelmvf@gmail.com>
2025-08-09 20:51:04 -0300

    feat: Add method to UserRepository for retrieving active users by username

--- src/main/java/br/com/astro/operations/repository/UserRepository.java
+++ src/main/java/br/com/astro/operations/repository/UserRepository.java
@@ -1,12 +1,16 @@
 package br.com.astro.operations.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.astro.operations.domain.entity.UserEntity;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+    @Query("SELECT u FROM UserEntity u WHERE u.username = :username AND u.deleted = false")
+    Optional<UserEntity> findActiveByUsername(String username);
 }
