198022ddf61a4ddf0e5b5fb725fa553984c20601
Samuel Maia <samuelmvf@gmail.com>
2025-08-12 02:28:39 -0300

    chore: Update application.yml for JWT secret length and default user email, remove obsolete import.sql file

--- src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO operations (id, type, cost, created_at, updated_at) VALUES (UUID(), 'ADDITION', 1.00, NOW(), NOW());
-INSERT INTO operations (id, type, cost, created_at, updated_at) VALUES (UUID(), 'SUBTRACTION', 1.00, NOW(), NOW());
-INSERT INTO operations (id, type, cost, created_at, updated_at) VALUES (UUID(), 'MULTIPLICATION', 2.00, NOW(), NOW());
-INSERT INTO operations (id, type, cost, created_at, updated_at) VALUES (UUID(), 'DIVISION', 2.00, NOW(), NOW());
-INSERT INTO operations (id, type, cost, created_at, updated_at) VALUES (UUID(), 'SQUARE_ROOT', 3.00, NOW(), NOW());
-INSERT INTO operations (id, type, cost, created_at, updated_at) VALUES (UUID(), 'RANDOM_STRING', 5.00, NOW(), NOW());
