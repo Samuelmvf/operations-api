96eb177dbea640a5a6681816498de08563691925
Samuel Maia <samuelmvf@gmail.com>
2025-08-12 01:19:01 -0300

    chore: Add Spring Security and JWT dependencies for enhanced security features

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,11 +34,16 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
-	
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
 	implementation("io.github.resilience4j:resilience4j-spring-boot3:$resilience4jVersion")
 	implementation("io.github.resilience4j:resilience4j-circuitbreaker:$resilience4jVersion")
 	implementation("io.github.resilience4j:resilience4j-retry:$resilience4jVersion")
 		
+	implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 
