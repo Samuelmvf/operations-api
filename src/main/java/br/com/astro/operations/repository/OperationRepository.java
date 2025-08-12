2cf1fe9be8c52bd98301da904cbd6958bfa63b6f
Samuel Maia <samuelmvf@gmail.com>
2025-08-11 23:57:12 -0300

    feat: Add method to OperationRepository for finding OperationEntity by type

--- src/main/java/br/com/astro/operations/repository/OperationRepository.java
+++ src/main/java/br/com/astro/operations/repository/OperationRepository.java
@@ -1,5 +1,6 @@
 package br.com.astro.operations.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,4 +10,5 @@ import br.com.astro.operations.domain.entity.OperationEntity;
 
 @Repository
 public interface OperationRepository extends JpaRepository<OperationEntity, UUID> {
+    Optional<OperationEntity> findByType(OperationEntity.OperationType type);
 }
