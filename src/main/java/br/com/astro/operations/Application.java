a0084cdc7cfaca81d779763754bbfed3a0d27f7e
Samuel Maia <samuelmvf@gmail.com>
2025-08-12 23:44:01 -0300

    feat: Enhance security configuration with additional HTTP headers and enable Spring Data web support

--- src/main/java/br/com/astro/operations/Application.java
+++ src/main/java/br/com/astro/operations/Application.java
@@ -3,9 +3,12 @@ package br.com.astro.operations;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class Application {
 
 	public static void main(String[] args) {
